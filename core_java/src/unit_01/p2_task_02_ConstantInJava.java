046440484592ce730e37d8980e6bb221b287fe9e
FAMILY <FAMILY@DESKTOP-MVCM1B2>
2022-03-25 15:55:21 +0630

    4th commit

--- core_java/src/unit_01/p2_task_02_ConstantInJava.java
+++ core_java/src/unit_01/p2_task_02_ConstantInJava.java
@@ -1,5 +1,30 @@
 package unit_01;
 
 public class p2_task_02_ConstantInJava {
+	public static final double Pi = 3.14;	
+	public static void main(String[] args) {
+			System.out.println(Pi);
+			main("Hello World");
+			
+			NewClass obj = new NewClass();
+			obj.add(1,2);
+			System.out.println(obj.Pi);
+		}
+	public static void main(String args) {
+		System.out.println(Pi);
+	}
+	
+	void add (int a, int b) {
+		System.out.println(a+b);
+		System.out.println(Pi);
+	}
+}
 
+class NewClass{
+	//final double Pi = 3.14;
+	
+	void add(int a,int b) {
+		System.out.println(a+b);
+		System.out.println(p2_task_02_ConstantInJava.Pi);
+	}
 }
