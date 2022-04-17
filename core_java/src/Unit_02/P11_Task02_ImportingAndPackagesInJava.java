6dc4b9ff5357e1c1bf0f74b4e55383a96fcdad2c
FAMILY <FAMILY@DESKTOP-MVCM1B2>
2022-04-17 20:53:38 +0630

    17 april

--- core_java/src/Unit_02/P11_Task02_ImportingAndPackagesInJava.java
+++ core_java/src/Unit_02/P11_Task02_ImportingAndPackagesInJava.java
@@ -1,5 +1,37 @@
-package Unit_02;
+package Unit_02;	//Inside this package we cab create many classes!
 
+/*
+- A java package is a group of similar types of classes, interface and sub-packages.
+- Package in java can be categorised in two form 
+	- built-in package {lang, awt, javax, swing, net, io, util, sql}
+	-user defined package
+	
+-	Package also helps to avoid class name collision
+- 	Package also helps us to maintain access protection in Java
+-	Packages are named in reverse order of domain names 
+ -	unit_01.javaproject.com -> "com.javaproject.unit01"
+ 
+import java.util.vector;   //import hte vector class from util package 
+import java.util.*;		  // import all the classes from util package 
+
+Static Import:
+- Static import is a feature introduced in Java Programming language (versions 5 and above)
+- That allows members (fields and methods) defined in class as public static, to be used in Java
+code without specifying the class in which the field is defined
+ * */
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+import unit_01.SampleClass1;
 public class P11_Task02_ImportingAndPackagesInJava {
-
+		public static void main(String args[]) {
+			out.println("Welcome to package");
+			
+			out.println(sqrt(4));
+			System.out.println(pow(2,2));
+			System.out.println(abs(6.3));
+			
+			SampleClass1 obj = new SampleClass1();
+			System.out.println(obj.a);
+		}
 }
--- core_java/src/unit_01/SampleClass1.java
+++ core_java/src/unit_01/SampleClass1.java
@@ -1,8 +1,8 @@
 package unit_01;
 
 public class SampleClass1 {
+	
 	public static void main(String [] args) {
-		int a = 10;
-		System.out.println("In unit 01 " +a+"\n");
+
 	}
 }
